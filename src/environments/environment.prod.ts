e7c7204ca9711641e6f6436540174469833688d8
darwin320 <darwinleonardovargas@gmail.com>
2023-05-14 01:16:15 -0500

    despliegue4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'https://server-production-b300.up.railway.app',
+  apiUrl: "https://server-production-b300.up.railway.app/api"
 };
